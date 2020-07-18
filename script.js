47739663b2b546a1f9c936ef16c6d4390aae1b72
William Campbell <campbellwilliam94@gmail.com>
2020-07-18 17:04:36 -0400

    Added start screen to add players, adjusted win/tie logic, styled new start form.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Player factory function handles creation of player objects
 const Player = (name, symbol) => {
 	const getName = () => name;
-	const getSymbol = symbol || "O";
+	const getSymbol = symbol;
 	let moves = [];
 
 	const addMoves = (index) => {
@@ -29,27 +29,56 @@ const Player = (name, symbol) => {
 const GameBoard = (() => {
 	const gameBoard = Array.from(document.querySelectorAll(".cell"));
 	const resetButton = document.querySelector(".reset");
+	const winBox = document.createElement("div");
+	winBox.classList.add("win-box");
+
+	const message = document.createElement("h1");
+	message.style.color = "white";
+	resetButton.style.visibility = "hidden";
 
 	const reset = () => {
 		gameBoard.forEach((cell) => (cell.innerText = ""));
-		GameFlow.players.forEach((player) => player.clearMoves());
+		GameFlow.resetPlayers();
 		GameFlow.resetTurn();
+		if (document.querySelector(".win-box") != undefined) {
+			document.body.removeChild(document.querySelector(".win-box"));
+		}
 	};
 	//Event Listener for play
 	gameBoard.forEach((cell, index) => {
 		cell.addEventListener("click", () => {
+			//cannot play a played ce;;
 			if (cell.innerText != "") return;
 			else {
-				//Add play to screen
-				cell.innerText = GameFlow.currentPlayer().getSymbol;
-				//Add move to players array
-				GameFlow.currentPlayer().addMoves(index);
-				//change current turn
-				GameFlow.changeTurn();
-
-				//TODO display on screen instad of console
+				//displays Win message and prevents additional plays after a win
 				if (GameFlow.checkWin() != undefined) {
-					console.log(GameFlow.checkWin());
+					message.innerText = `${GameFlow.checkWin()} wins`;
+
+					document.body.appendChild(winBox);
+					winBox.appendChild(message);
+				} else {
+					//Add play to screen
+					cell.innerText = GameFlow.currentPlayer().getSymbol;
+					if (cell.innerText == "x") {
+						cell.style.color = "#2399c4";
+					} else cell.style.color = "#eb532d";
+
+					//Add move to players array
+					GameFlow.currentPlayer().addMoves(index);
+					//change current turn
+					GameFlow.changeTurn();
+
+					//Additional win check so the event listener
+					//does not have to be ran again to see the game has been won
+					if (GameFlow.checkWin() != undefined) {
+						//Displays Tie if its a tie otherwise the winner
+						if (GameFlow.checkWin() != "Tie") {
+							message.innerText = `${GameFlow.checkWin()} wins`;
+						} else message.innerText = GameFlow.checkWin();
+
+						document.body.appendChild(winBox);
+						winBox.appendChild(message);
+					}
 				}
 			}
 		});
@@ -59,26 +88,55 @@ const GameBoard = (() => {
 	resetButton.addEventListener("click", reset);
 
 	return {
+		reset,
 		gameBoard,
+		resetButton,
 	};
 })();
 
 //GameFlow module controls the turns
-//checksfor wins and ties
-//Resets game
+//checksfor wins and ties & Resets game
 const GameFlow = (() => {
-	//TODO create these players from a form on the page instead of hard coding them in
-	const players = [
-		(player1 = Player("Will", "x")),
-		(computer = Player("computer", "o")),
-	];
+	const boardDisplay = document.querySelector(".game-board");
+	boardDisplay.style.visibility = "hidden";
+
+	const formX = document.querySelector('[name="player-x"]');
+	const formO = document.querySelector('[name="player-o"]');
+
+	const setupContainer = document.getElementById("setup-container");
+	const start = document.getElementById("start-btn");
+
+	const failedForm = document.createElement("p");
+	failedForm.classList.add("failed-form");
+
+	var playerX;
+	var playerO;
+	var players;
+
+	start.addEventListener("click", () => {
+		//form validation
+		if (formX.value == "" || formO.value == "") {
+			failedForm.innerText = "Please enter a value for both X and O";
+			setupContainer.insertBefore(failedForm, start);
+		} else {
+			//save value into variable to create objects with in GameFlow
+			playerX = Player(formX.value, "x");
+			playerO = Player(formO.value, "o");
+			players = [playerX, playerO];
+
+			//Hide start display and button, reveal gameboard
+			setupContainer.style.visibility = "hidden";
+			boardDisplay.style.visibility = "visible";
+			GameBoard.resetButton.style.visibility = "visible";
+		}
+	});
 
 	let turn = 1;
 
 	const resetTurn = () => (turn = 1);
+	const resetPlayers = () => players.forEach((player) => player.clearMoves());
 	const checkWin = () => {
 		//save current state of gamebBoard into array & check cells for win
-
 		let player1Moves = players[0].getMoves();
 		let player2Moves = players[1].getMoves();
 
@@ -92,9 +150,6 @@ const GameFlow = (() => {
 			[0, 4, 8],
 			[2, 4, 6],
 		];
-
-		//Player moves array can be larger than 3
-		//players moves must include the three values
 
 		const matchCheck = (player) => {
 			return winPatterns.some((pattern) => {
@@ -125,5 +180,6 @@ const GameFlow = (() => {
 		changeTurn,
 		checkWin,
 		resetTurn,
+		resetPlayers,
 	};
 })();
